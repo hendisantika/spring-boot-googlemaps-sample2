9a55d7972aab8b4a6ed6422311cafe8e0b74ca08
hendisantika <hendisantika@yahoo.co.id>
2023-03-04 06:03:49 +0700

    #10 - Add JobPosition Entity ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootgooglemapssample2/entity/JobPosition.java
@@ -0,0 +1,15 @@
+package com.hendisantika.springbootgooglemapssample2.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-googlemaps-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/4/23
+ * Time: 06:03
+ * To change this template use File | Settings | File Templates.
+ */
+public enum JobPosition {
+    JUNIOR, MEDIOR, SENIOR, ADMIN, HR
+}
